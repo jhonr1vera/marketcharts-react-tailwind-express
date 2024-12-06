6414c5afd9aa8f4c6fab891c9efe3ecb05bfb060
Jhon Rivera <jhonariverab@gmail.com>
2024-12-06 16:13:51 -0400

    add env

--- server/config/dbconfig.js
+++ server/config/dbconfig.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const connection = mysql.createConnection({
     host: process.env.DB_NAME,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASS,
     database: 'instituto_tesis',
     charset: 'utf8mb4'
 });
